1aa96c0072ba287fa7fe3f82e79d1ea1289af3ef
Vyacheslav <dedmopoz.bakemono@gmail.com>
2021-06-07 18:40:11 +0300

    added accounts app in django added templates to signup, login, change password

---
+++ src/accounts/views.py
@@ -0,0 +1,11 @@
+from django.urls import reverse_lazy
+from django.views import generic
+
+from .forms import NewUserForm
+
+
+class SignUpView(generic.CreateView):
+    form_class = NewUserForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
